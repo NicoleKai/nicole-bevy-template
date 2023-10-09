595ffa8ca8ec293061571f9968275298b5164b9b
NicoleKai <nicolekohm102@gmail.com>
2023-10-09 00:15:17 -0700

    i like doughnuts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::default;
+use std::f32::consts::PI;
 
 use bevy::{prelude::*, window::WindowPlugin, DefaultPlugins};
+
+#[derive(Component)]
+struct Rotatable;
 
 fn setup(
     mut commands: Commands,
@@ -15,15 +18,30 @@ fn setup(
     let my_material = materials.add(Color::WHITE.into());
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0.0, 10.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
-    commands.spawn(PbrBundle {
-        mesh: my_mesh,
-        material: my_material,
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 10.0),
         ..default()
     });
+
+    commands.spawn((
+        PbrBundle {
+            mesh: my_mesh,
+            material: my_material,
+            ..default()
+        },
+        Rotatable,
+    ));
+}
+
+fn system_continuous_rotate(mut rotatables: Query<(&mut Rotatable, &mut Transform)>) {
+    for (_rotatable, mut transform) in rotatables.iter_mut() {
+        transform.rotate_x(PI * 0.001);
+        transform.rotate_y(PI * 0.001);
+    }
 }
 
 fn main() {
@@ -36,5 +54,6 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(Update, system_continuous_rotate)
         .run();
 }
